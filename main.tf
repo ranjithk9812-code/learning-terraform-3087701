17ad81e83797dc3207b94d9ee6e467d3a5a3d3bb
ranjithk9812-code <ranjithk9812@gmail.com>
2026-06-23 19:57:45 +0000

    added AWS instance

--- main.tf
+++ main.tf
@@ -21,4 +21,12 @@ data "aws_ami" "app_ami" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+  resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
 }
