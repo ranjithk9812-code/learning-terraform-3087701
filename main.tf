00357bc485dec16e6ea04c7d101edca84e8d6155
ranjithk9812-code <ranjithk9812@gmail.com>
2026-06-23 20:03:11 +0000

    AWS instances updated

--- main.tf
+++ main.tf
@@ -21,12 +21,13 @@ data "aws_ami" "app_ami" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  resource "aws_instance" "web" {
+}
+
+resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
   tags = {
     Name = "HelloWorld"
   }
-}
 }
