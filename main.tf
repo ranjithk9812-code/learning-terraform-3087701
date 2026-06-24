1ed89cd682af0f0e40964d58da36a4ecae0f5291
ranjithk9812-code <ranjithk9812@gmail.com>
2026-06-24 20:36:50 +0000

    removed the security group

--- main.tf
+++ main.tf
@@ -23,53 +23,9 @@ data "aws_ami" "app_ami" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-module "web_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "6.0.0"
-
-  name        = "web-new-sg"
-  description = "Allow HTTP and HTTPS inbound and all outbound"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress_rules = {
-    http = {
-      from_port   = 80
-      to_port     = 80
-      ip_protocol = "tcp"
-      cidr_ipv4   = "0.0.0.0/0"
-      description = "Allow HTTP"
-    }
-
-    https = {
-      from_port   = 443
-      to_port     = 443
-      ip_protocol = "tcp"
-      cidr_ipv4   = "0.0.0.0/0"
-      description = "Allow HTTPS"
-    }
-  }
-
-  egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "0.0.0.0/0"
-      description = "Allow all outbound"
-    }
-  }
-
-  tags = {
-    Name = "web-new-sg"
-  }
-}
-
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.web_sg.id]
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
   tags = {
     Name = "HelloWorld"
