83af5f75229ace0d7a40622e88955b96c16d9261
ranjithk9812-code <ranjithk9812@gmail.com>
2026-06-24 13:46:37 +0000

    same update the output file

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,19 @@
 output "instance_ami" {
-  value = aws_instance.blog.ami
+  value = aws_instance.web.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.blog.arn
+  value = aws_instance.web.arn
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "public_dns" {
+  value = aws_instance.web.public_dns
+}
+
+output "instance_id" {
+  value = aws_instance.web.id
 }
