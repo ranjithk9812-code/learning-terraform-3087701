46329131d33fcb8e48d1420151ad793ae092b6e3
ranjithk9812-code <ranjithk9812@gmail.com>
2026-06-24 20:20:12 +0000

    use module for security group

--- main.tf
+++ main.tf
@@ -27,47 +27,29 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_security_group" "web" {
-  name        = "web-sg"
+module "web_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "6.0.0"
+
+  name        = "web-new-sg"
   description = "Allow HTTP and HTTPS inbound and all outbound"
   vpc_id      = data.aws_vpc.default.id
 
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
   tags = {
-    Name = "web-sg"
+    Name = "web-new-sg"
   }
-}
-
-resource "aws_security_group_rule" "web_http_in" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_https_in" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_everything_out" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.web.id
 }
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.web.id]
+  vpc_security_group_ids = [module.web_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
