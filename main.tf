2355d94e76c954e09b2538de4bdf0b8892a8b886
ranjithk9812-code <ranjithk9812@gmail.com>
2026-06-25 15:34:39 +0000

    Added the module VPC

--- main.tf
+++ main.tf
@@ -22,10 +22,30 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 }
+ module "web_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
 
+  name = "mydev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                         = data.aws_ami.app_ami.id
+  instance_type               = var.instance_type
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
 
   tags = {
     Name = "HelloWorld"
